a4d7e797b718ae8673f935155d20ba1a012c844b
Patricio Huang <patriciohuang20@gmail.com>
2023-04-09 12:16:37 +0200

    Take github link out

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -28,9 +28,6 @@
                     </div>
                 </v-card>    
                 <div id="links">
-                    <a href="https://github.com/patriciohuang/home-stock-manager" target="_blank">
-                        <v-icon icon="mdi-github"></v-icon>
-                    </a>
                     <a href="https://home-stock-manager.vercel.app" target="_blank">
                         <v-icon icon="mdi-open-in-new"></v-icon>
                     </a>
